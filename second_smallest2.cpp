4f70fb8f6432908b04a4821b227573ea174cdfcf
Tanvi751 <119522454+Tanvi751@users.noreply.github.com>
2022-11-30 22:40:20 +0530

    Add files via upload

---
+++ second_smallest2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{  int array[10]={12,34,23,9,45,90,11,98,80,43};
+   int min=array[0];
+   for(int i=0;i<10;i++)
+   {
+      
+   }
+
+
+
+
+    return 0;
+}
